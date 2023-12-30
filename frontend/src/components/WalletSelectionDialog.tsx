edff037d4db5c18bf958d4d489625926284ffc8c
Tarang Patel <tarang5757@gmail.com>
2023-12-30 17:07:01 -0500

    added comments in walletSelection

--- frontend/src/components/WalletSelectionDialog.tsx
+++ frontend/src/components/WalletSelectionDialog.tsx
@@ -1,3 +1,4 @@
+// Importing required components and functions from Material-UI and custom services
 import { Button, Dialog, Stack } from "@mui/material";
 import { connectToBladeWallet } from "../services/wallets/blade/bladeClient";
 import { hashConnect } from "../services/wallets/hashconnect/hashconnectClient";
@@ -5,24 +6,31 @@ import { connectToMetamask } from "../services/wallets/metamask/metamaskClient";
 import HashPackLogo from "../assets/hashpack-logo.svg";
 import MetamaskLogo from "../assets/metamask-logo.svg";
 
-
+// Define the props for the WalletSelectionDialog component
 interface WalletSelectionDialogProps {
   open: boolean;
   onClose: (value: string) => void;
 }
 
+// WalletSelectionDialog component for displaying wallet options
 export const WalletSelectionDialog = (props: WalletSelectionDialogProps) => {
+  // Destructure props to get open and onClose
   const { onClose, open } = props;
 
   return (
+    // Dialog component from Material-UI for displaying a modal
     <Dialog onClose={onClose} open={open}>
+      {/* Stack component to organize buttons in a vertical stack */}
       <Stack p={2} gap={1}>
+        {/* Button for connecting to HashPack wallet */}
         <Button
           variant="contained"
           onClick={() => {
+            // Call the connectToLocalWallet function from hashConnect service
             hashConnect.connectToLocalWallet();
           }}
         >
+          {/* HashPack wallet logo */}
           <img
             src={HashPackLogo}
             alt='hashpack logo'
@@ -33,20 +41,25 @@ export const WalletSelectionDialog = (props: WalletSelectionDialogProps) => {
           />
           HashPack
         </Button>
+        {/* Button for connecting to Blade wallet */}
         <Button
           variant="contained"
           onClick={() => {
+            // Call the connectToBladeWallet function
             connectToBladeWallet();
           }}
         >
           Blade
         </Button>
+        {/* Button for connecting to Metamask wallet */}
         <Button
           variant="contained"
           onClick={() => {
+            // Call the connectToMetamask function
             connectToMetamask();
           }}
         >
+          {/* Metamask wallet logo */}
           <img
             src={MetamaskLogo}
             alt='metamask logo'
