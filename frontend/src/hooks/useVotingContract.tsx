d9c2939219792e73c5c3edcf9c61679ed53ed60d
lordforever <shashanktrivedi1917@gmail.com>
2023-12-31 06:02:15 +0530

    ether functions

--- frontend/src/hooks/useVotingContract.tsx
+++ frontend/src/hooks/useVotingContract.tsx
@@ -1,51 +1,51 @@
-import { ContractFunctionParameterBuilder } from "../services/wallets/contractFunctionParameterBuilder";
-import { useWalletInterface } from "../services/wallets/useWalletInterface";
 import { contractAddress, abi } from "../constants";
-import { ContractId } from "@hashgraph/sdk";
+const ethers = require("ethers");
+const { ethereum } = window as any;
+
+const getProvider = () => {
+  if (!ethereum) {
+    throw new Error("Metamask is not installed! Go install the extension!");
+  }
+
+  return new ethers.providers.Web3Provider(ethereum);
+};
+
 const useVoteContract = () => {
-  const { walletInterface } = useWalletInterface();
+  const provider = getProvider();
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(
+    contractAddress.Voting,
+    abi.Voting,
+    signer
+  );
   const addProposal = async (proposal: string) => {
-    await walletInterface?.executeContractFunction(
-      ContractId.fromSolidityAddress(contractAddress.Voting),
-      "addProposal",
-      new ContractFunctionParameterBuilder().addParam({
-        type: "string",
-        name: "proposal",
-        value: proposal,
-      }),
-      Number("1000000")
-    );
-    return "proposal added";
+    try {
+      const tx = await contract.addProposal(proposal, { gasLimit: 1000000 });
+      await tx.wait();
+      return "proposal added";
+    } catch (error) {
+      console.error("Error adding proposal: ", error);
+    }
   };
 
   const vote = async (proposal: string, votes: number) => {
-    await walletInterface?.executeContractFunction(
-      ContractId.fromSolidityAddress(contractAddress.Voting),
-      "vote",
-      new ContractFunctionParameterBuilder()
-        .addParam({
-          type: "string",
-          name: "proposal",
-          value: proposal,
-        })
-        .addParam({
-          type: "uint",
-          name: "votes",
-          value: votes,
-        }),
-      Number("1000000")
-    );
-    return "voted";
+    try {
+      const tx = await contract.vote(proposal, votes, { gasLimit: 1000000 });
+      await tx.wait();
+      return "voted";
+    } catch (error) {
+      console.error("Error voting: ", error);
+    }
   };
 
   const distributeFunds = async () => {
-    await walletInterface?.executeContractFunction(
-      ContractId.fromSolidityAddress(contractAddress.Voting),
-      "distributeFunds",
-      new ContractFunctionParameterBuilder(),
-      Number("1000000")
-    );
-    return "funds distributed";
+    try {
+      const tx = await contract.distributeFunds({ gasLimit: 1000000 });
+      await tx.wait();
+      return "funds distributed";
+    } catch (error) {
+      console.error("Error distributing funds: ", error);
+    }
   };
 
   return { addProposal, vote, distributeFunds };
