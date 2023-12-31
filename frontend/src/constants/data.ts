9fb85f545604f64f7f1f14760d829eab6dc8a232
araf821 <araf.ahmed200@gmail.com>
2023-12-31 08:25:23 -0500

    refactor: improved styles

--- frontend/src/constants/data.ts
+++ frontend/src/constants/data.ts
@@ -71,3 +71,92 @@ export const proposals = [
     votingFactor: 2,
   },
 ];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Projekt Bebicat",
+    imageUrl:
+      "https://images.unsplash.com/photo-1496065187959-7f07b8353c55?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "A groundbreaking project focused on innovative technology.",
+    externalLinks: [
+      "https://github.com/bebicat",
+      "https://youtube.com/bebicat",
+    ],
+    totalVotes: 13,
+  },
+  {
+    id: 2,
+    title: "EcoSolutions",
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1683121528306-59d7e562789a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Dedicated to creating sustainable environmental practices.",
+    externalLinks: [
+      "https://github.com/ecosolutions",
+      "https://youtube.com/ecosolutions",
+    ],
+    totalVotes: 27,
+  },
+  {
+    id: 3,
+    title: "TechAdvancers",
+    imageUrl:
+      "https://images.unsplash.com/photo-1518770660439-4636190af475?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Pushing the boundaries of technology for a better tomorrow.",
+    externalLinks: [
+      "https://github.com/techadvancers",
+      "https://youtube.com/techadvancers",
+    ],
+    totalVotes: 42,
+  },
+  {
+    id: 4,
+    title: "Health Innovate",
+    imageUrl:
+      "https://images.unsplash.com/photo-1485827404703-89b55fcc595e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Revolutionizing healthcare through cutting-edge research.",
+    externalLinks: [
+      "https://github.com/healthinnovate",
+      "https://youtube.com/healthinnovate",
+    ],
+    totalVotes: 36,
+  },
+  {
+    id: 5,
+    title: "GreenWorld Initiatives",
+    imageUrl:
+      "https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Focusing on making the planet greener and cleaner.",
+    externalLinks: [
+      "https://github.com/greenworld",
+      "https://youtube.com/greenworld",
+    ],
+    totalVotes: 21,
+  },
+  {
+    id: 6,
+    title: "AI Pioneers",
+    imageUrl:
+      "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description:
+      "Leading the way in artificial intelligence and machine learning.",
+    externalLinks: [
+      "https://github.com/aipioneers",
+      "https://youtube.com/aipioneers",
+    ],
+    totalVotes: 55,
+  },
+  {
+    id: 7,
+    title: "Space Explorers",
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1663091701962-2ae72a2ad2ac?q=80&w=2087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description:
+      "Exploring the final frontier with innovative space technologies.",
+    externalLinks: [
+      "https://github.com/spaceexplorers",
+      "https://youtube.com/spaceexplorers",
+    ],
+    totalVotes: 30,
+  },
+];
--- frontend/src/pages/ProposalPage.tsx
+++ frontend/src/pages/ProposalPage.tsx
@@ -1,7 +1,8 @@
-import { ArrowBack } from "@mui/icons-material";
+import { AddCircle, ArrowBack } from "@mui/icons-material";
 import AnimatedHr from "../components/AnimatedHr";
 import { useNavigate, useParams } from "react-router-dom";
-import { proposals } from "../constants/data";
+import { projects, proposals } from "../constants/data";
+import { Button } from "@mui/material";
 
 interface ProposalPageProps {}
 
@@ -16,16 +17,21 @@ const ProposalPage = ({}: ProposalPageProps) => {
   if (!proposal) return null;
 
   return (
-    <div className="max-w-screen-lg px-4 mx-auto mt-8">
+    <div className="max-w-screen-lg mb-8 px-4 mx-auto mt-8">
       <button
         className="mb-8 p-2 bg-purple-900/10 rounded-md transition text-zinc-400 hover:text-zinc-100 hover:bg-purple-900/30"
         onClick={() => navigate(-1)}
       >
         <ArrowBack fontSize="medium" />
       </button>
-      <h1 className="text-2xl mb-4 md:text-3xl font-medium w-full">
-        {proposal.name}
-      </h1>
+      <div className="flex mb-4 gap-4 max-md:flex-col justify-between items-center">
+        <h1 className="text-2xl md:text-3xl font-medium w-full">
+          {proposal.name}
+        </h1>
+        <Button className="max-md:w-full" size="small" variant="outlined">
+          Distribute Prizes
+        </Button>
+      </div>
 
       <AnimatedHr />
 
@@ -39,6 +45,95 @@ const ProposalPage = ({}: ProposalPageProps) => {
         purr as loud as possible, be the most annoying cat that you can, and,
         knock everything off the table.
       </p>
+
+      <div className="mt-4 grid grid-cols-2 md:grid-cols-4 gap-4">
+        <div className="aspect-video rounded-lg border-purple-600/30 relative grid place-items-center border-2 p-2">
+          <span className="max-md:text-xs text-sm absolute top-2 left-2">
+            Voting Factor
+          </span>
+          <span className="text-xl text-purple-500 max-md:text-lg font-medium tracking-widest">
+            {proposal.votingFactor}
+          </span>
+        </div>
+
+        <div className="aspect-video rounded-lg border-purple-600/30 relative grid place-items-center border-2 p-2">
+          <span className="max-md:text-xs text-sm absolute top-2 left-2">
+            Total Funds
+          </span>
+          <span className="text-xl text-purple-500 max-md:text-lg font-medium tracking-widest">
+            {proposal.totalFunds}
+          </span>
+        </div>
+
+        <div className="aspect-video rounded-lg border-purple-600/30 relative grid place-items-center border-2 p-2">
+          <span className="max-md:text-xs text-sm absolute top-2 left-2">
+            Total Votes Per Voter
+          </span>
+          <span className="text-xl text-purple-500 max-md:text-lg font-medium tracking-widest">
+            {proposal.totalVotesPerVoter}
+          </span>
+        </div>
+
+        <div className="aspect-video rounded-lg border-purple-600/30 relative grid place-items-center border-2 p-2">
+          <span className="max-md:text-xs text-sm absolute top-2 left-2">
+            Project Limit
+          </span>
+          <span className="text-xl text-purple-500 max-md:text-lg font-medium tracking-widest">
+            {proposal.projectLimit}
+          </span>
+        </div>
+      </div>
+
+      <h2 className="mt-8 mb-2 text-xl md:text-2xl">Projects</h2>
+      <AnimatedHr />
+
+      <div className="grid mt-6 gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {projects.map((project) => (
+          <div
+            key={project.id}
+            role="button"
+            onClick={() =>
+              navigate(
+                `/proposals/${name}/${project.title.replaceAll(" ", "-")}`
+              )
+            }
+            className="border relative transition hover:scale-110 border-purple-900/50 aspect-[11/16] flex flex-col"
+          >
+            <div className="aspect-[16/10] relative">
+              <img
+                src={project.imageUrl}
+                alt={project.description}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-full flex-col flex items-center flex-1">
+              <p className="mt-4 drop-shadow-md">{project.title}</p>
+
+              <span className="text-xs text-zinc-400 mt-8">TOTAL VOTES</span>
+              <p className="font-semibold mb-2 tracking-widest text-purple-700">
+                {project.totalVotes}
+              </p>
+              <div className="absolute left-1 right-1 bottom-1">
+                <Button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    alert("Voted!");
+                  }}
+                  className="w-full"
+                  variant="outlined"
+                  size="small"
+                >
+                  Vote
+                </Button>
+              </div>
+            </div>
+          </div>
+        ))}
+        <button className="group flex gap-4 hover:scale-110 transition flex-col justify-center items-center border aspect-[11/16] border-purple-900">
+          <p className="transition group-hover:text-purple-500">Add Project</p>
+          <AddCircle className="transition group-hover:text-purple-500" />
+        </button>
+      </div>
     </div>
   );
 };
